521ffe38dcd6f45875511d1ed8ff5e8eef2b2bb1
frantz-torry <frantztorry@gmail.com>
2025-04-17 19:07:59 -0400

    Week 7 + Screenshot

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+       List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2024", 30, 3.0));
+        activities.Add(new Cycling("03 Nov 2024", 45, 15.0));
+        activities.Add(new Swimming("03 Nov 2024", 40, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
+
 }
